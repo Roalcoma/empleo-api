7d5d7f678077669ddd854e4ce756523a23f09ae8
Rodrigo <rodrigoalfonzo97@gmail.com>
2025-10-11 01:16:51 -0400

    Se agregó un versionamiento basico

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors(); // Habilitar CORS
+  app.setGlobalPrefix('api'); // Establecer prefijo global
+
+  app.enableVersioning({
+    type: VersioningType.URI, // URI Versioning
+    defaultVersion: '1',
+  })
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,10 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { UsersService } from './users.service';
 
-@Controller('usuarios')
+@Controller({
+    path: 'usuarios',
+    version: '1'
+})
 export class UsersController {
     constructor(private readonly usersService: UsersService) {}
 
