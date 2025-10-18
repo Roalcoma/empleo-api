0561dce58d1dc3fb4f6037972d11f4e018631e6e
Rodrigo <rodrigoalfonzo97@gmail.com>
2025-10-17 23:04:14 -0400

    Commit 17-10-2025: Se programaron los controladores

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, HttpCode, HttpStatus } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
-@Controller('auth')
-export class AuthController {}
+@Controller({
+    path: 'auth',
+    version: '1'
+})
+export class AuthController {
+    constructor(
+        private readonly authService: AuthService
+    ) {}
+
+    @HttpCode(HttpStatus.OK)
+    @Post('login')
+    async login(@Body() loginDto: any) {
+        const resultado = await this.authService.login(loginDto);
+
+        return resultado;
+    }
+}
